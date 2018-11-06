8ea92bed5381518580f73400994db1b5ea8b4f02
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-11-05 20:02:22 -0600

    mess around a bit- commented out async and defer

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ componentDidMount() {
 initMap=()=> {
    let map= new window.google.maps.Map(document.getElementById('map'), {
    center: {lat: 44.519159, lng: -88.019826}, 
-   zoom: 14})  
+   zoom: 12})  
   }	
 
 
@@ -35,8 +35,7 @@ function loadMap(url) { //help from Yahya Elharony
 	let index = window.document.getElementsByTagName("script")[0]
 	let script= window.document.createElement("script")
 	script.src = url
-	script.async=true
-	script.defer=true
+	//script.async = true script.defer= true
 	index.parentNode.insertBefore(script,index)
 }
 export default App;
