70b51ae7d96fd35cc5d77a94a94fd0e97515a491
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-11-09 12:23:55 -0600

    add use of response- uncomment componentDidMount

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,20 +5,20 @@ import axios from 'axios'
 
 class Restaurants extends Component{
 
-//componentDidMount(){
-	//this.createMarkers()
-//}
+componentDidMount(){
+	this.createMarkers()
+}
 //still works
 	
 
 
 createMarkers = () => {
-  let url = "https://api.myjson.com/bins/kqq1y"
-  axios.get(url)
+ // let url = ""
+  axios.get(`https://api.myjson.com/bins/kqq1y`)
   .then(response => {
     console.log(response)
     //this.state.locations.push(this.locations)
-    this.setState({locations: [this.state.locations, this.locationData]});
+    this.setState({locations: response.data});
   }).catch(function(error){
     console.log(error);
   })/*named array:"https://api.myjson.com/bins/sg80u"
