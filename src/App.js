46e4edac471ef7199c806de101e66fed9e586959
JennaKoslowski <jenna.koslowski@yahoo.com>
2018-11-11 07:15:37 -0600

    data into state move everything into smaller components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@
 import React, { Component } from 'react'
 import Map from './components/Map'
 import Restaurants from './components/Restaurants'
-import locationData from './data/location.json'
 import './App.css'
-import GoogleMapReact from 'google-map-react'
 import SearchBar from './components/SearchBar'
 import axios from 'axios'
 
@@ -13,50 +11,32 @@ class App extends Component {
 state = {
 	markers: [],
 	locations: [],
-	foursquareInfo:[]
+	foursquareData:[]
 }
-
 componentDidMount() {
-	this.renderMap()
-	this.renderFoursquare()
+  this.renderFoursquare()
 }
 
-initMap=()=> {
-   let map= new window.google.maps.Map(document.getElementById('map'), {
-   center: {lat: 44.519159, lng: -88.019826}, 
-   zoom: 12});  
-  }	
-
-
-renderMap=()=>{
-	loadMap("https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyC5-0uFUUOwNG8qyC82A6zUbfD619EbDUw&callback=initMap")
-	window.initMap = this.initMap
-}
-
-/*createMarkers = () => {
-    let markers= new window.google.maps.Marker({
-     position: {lat: window.location.location.lat, lng: window.location.location.lng}
-    }); 
-    this.state.markers.push(this.markers);
-  }*/
-  renderFoursquare = (query) => {
-	const endpoint= "https://api.foursquare.com/v2/venues/explore?"
-	let foursquareInfo= { // missing access credentials
-		client_id: "5GRVKAT5C2F1EALKLBKBQQLJPSL2CKLUQXSQI3O3LHANQNK5",
-		client_secret: "GETW1UOWQITCGPGNCIDQTA3QSZ2PD1YCD4HFT3BCFE4MXXO4",
-		near: "Green Bay",
-		query: "coffee",
-		limit: 10,
-		radius: 16093.4,
-		v: "20181106"
-	}
-	axios.get(endpoint + new URLSearchParams(foursquareInfo))
-	.then(res => {
-		// this.setState({foursquareInfo: res.data});
-		 console.log(res)
-	}).catch(error=> {
-		console.log("Error:" +error)
-	}) 
+ renderFoursquare = (query) => { //need to use in both searchbar and restaurants
+  const endpoint= "https://api.foursquare.com/v2/venues/explore?"
+  let foursquareInfo= { 
+    client_id: "5GRVKAT5C2F1EALKLBKBQQLJPSL2CKLUQXSQI3O3LHANQNK5",
+    client_secret: "GETW1UOWQITCGPGNCIDQTA3QSZ2PD1YCD4HFT3BCFE4MXXO4",
+    near: "Green Bay",
+    query: "coffee",
+    limit: 10,
+    radius: 16093.4,
+    v: "20181106"
+  }
+  axios.get(endpoint + new URLSearchParams(foursquareInfo))
+  .then(res => {
+    const foursquareData = res.data.response.groups[0].items
+    this.setState({foursquareData: res.data.response.groups[0].items});
+    // console.log(res.data.response.groups[0].items)
+    //console.log({foursquareData})
+  }).catch(error=> {
+    console.log("Error:" +error)
+  }) 
 }
 
   render() {
@@ -76,11 +56,4 @@ renderMap=()=>{
   }
 }
 
-function loadMap(url) { //help from Yahya Elharony
-	let index = window.document.getElementsByTagName("script")[0]
-	let script= window.document.createElement("script")
-	script.src = url
-	//script.async = true script.defer= true
-	index.parentNode.insertBefore(script,index)
-}
 export default App;
--- src/components/Map.js
+++ src/components/Map.js
@@ -3,6 +3,20 @@ import React, { Component } from 'react'
 
 class Map extends Component {
   
+componentDidMount() {
+  this.renderMap()
+}
+
+initMap=()=> {
+   let map= new window.google.maps.Map(document.getElementById('map'), {
+   center: {lat: 44.519159, lng: -88.019826}, 
+   zoom: 12});  
+  }	
+
+renderMap=()=>{
+	loadMap("https://maps.googleapis.com/maps/api/js?v=3&key=AIzaSyC5-0uFUUOwNG8qyC82A6zUbfD619EbDUw&callback=initMap")
+	window.initMap = this.initMap
+}
 render () {
 
 return (
@@ -14,4 +28,13 @@ return (
 	)
 		}
 }
+
+function loadMap(url) { //help from Yahya Elharony
+	let index = window.document.getElementsByTagName("script")[0]
+	let script= window.document.createElement("script")
+	script.src = url
+	//script.async = true script.defer= true
+	index.parentNode.insertBefore(script,index)
+}
+
 export default Map;
